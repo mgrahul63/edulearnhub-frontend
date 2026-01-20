3e6291bf07b3074f6aa07443a2136d127a4458ee
mgrahul63 <mgrahul639@gmail.com>
2026-01-20 12:31:31 +0600

    fix the responsive design

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
       {/* Mobile Menu Overlay */}
       {menuOpen && (
         <div className="fixed inset-0 bg-black/40 z-40 md:hidden">
-          <div className="absolute top-0 right-0 w-50 bg-white z-50 p-3 flex flex-col gap-1 rounded-bl-xl">
+          <div className="absolute top-0 right-0 w-50 bg-white z-50 p-3 flex flex-col gap-1 rounded-bl-xl divide-y divide-gray-300">
             <button
               className="self-end text-xl"
               onClick={() => setMenuOpen(false)}
